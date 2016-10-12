b42c0204f46592d75f5d58c4f655298b1077e4d1
FizzBot007 <balitejunneil@yahoo.com>
2016-10-12 16:14:39 +0800

    Add files via upload

---
+++ lab9/lab9/Circle.java
@@ -0,0 +1,16 @@
+public class Circle implements GeometricObject{
+	protected double radius = 1.0;
+
+	public Circle(double radius){
+		this.radius = radius;
+	}
+	public double getPerimeter(){
+		return 2*Math.PI*radius;
+	}
+	public double getArea(){
+		return Math.PI*radius*radius;
+	}
+	public String toString(){
+		return String.format("radius: %.2f", radius);
+	}
+}
---
+++ lab9/lab9/MovableCircle.java
@@ -0,0 +1,29 @@
+
+public class MovableCircle implements Movable{
+	private MovablePoint center;
+	private int radius;
+
+	public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius){
+		center =  new MovablePoint(x, y, xSpeed, ySpeed);
+		this. radius = radius;
+	}
+
+	public void moveUp(){
+		center.y -= center.ySpeed;
+	}
+
+	public void moveDown(){
+		center.y += center.ySpeed;
+	}
+
+	public void moveLeft(){
+		center.x -= center.xSpeed;
+	}
+
+	public void moveRight(){
+		center.x += center.xSpeed;
+	}
+	public String toString(){
+		return String.format("x-coord: %d --- y-coord: %d --- radius %d", center.x,center.y,radius);
+	}
+}
